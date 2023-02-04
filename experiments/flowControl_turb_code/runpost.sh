b4e43b36ee29a5fe7fbf74e378383e191dde0031
rmojgani <rmojgani@gmail.com>
2023-02-04 21:35:43 +0000

    post process sh files added

---
+++ experiments/flowControl_turb_code/runpost.sh
@@ -0,0 +1,15 @@
+NLES=16
+case=1
+rewardtype=z1 # [k1,k2,k3,log,]
+statetype=enstrophy # [enstrophy,energy,psidiag,psiomegadiag,] 
+actiontype=CS
+gensize=10
+solver=postprocess #training #postproces
+nagents=16
+myoutfile=${solver}_CASE${case}_N${NLES}_R${rewardtype}_S${statetype}_A${actiontype}_nAgents${nagents}.out
+echo ${myoutfile}>>${myoutfile}
+ps>>${myoutfile}
+(ls -ltr|tail -n 1)>>${myoutfile}
+
+nohup python3 -u run-vracer-turb.py --case=${case} --rewardtype=${rewardtype} --statetype=${statetype} --actiontype=${actiontype} --NLES=${NLES} --gensize=${gensize} --solver=${solver} --nagents=${nagents}>> ${myoutfile}&
+
---
+++ experiments/flowControl_turb_code/viewout.sh
@@ -0,0 +1,1 @@
+grep -wrin "Average Reward for agent 0" training_CASE1_N16_Rz1_*.out
