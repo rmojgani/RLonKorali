39d590a0b2b4ec2dc8506e03ea0286f0c0ba7bda
rmojgani <rmojgani@gmail.com>
2023-02-22 04:57:46 +0000

    save numbering update

--- experiments/flowControl_turb_code/_model/environmentpost.py
+++ experiments/flowControl_turb_code/_model/environmentpost.py
@@ -4,6 +4,11 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams['image.cmap'] = 'bwr_r'
 import numpy as np
+# Loading files
+import glob
+import re
+import os
+import scipy.io as sio
 
 def environmentpost(args, s):
     L    = 2*np.pi
@@ -55,6 +60,23 @@ def environmentpost(args, s):
     print('run controlled simulation with nControlledSteps=', nContolledSteps)
     mystr = "smagRL"#'smag0d17'
     step = 0
+
+    try:
+        list_of_files = glob.glob(runFolder+'*.mat')
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print('Last file loaded:', latest_file)
+        mat_contents = sio.loadmat(latest_file)
+        sim.psiCurrent_hat = mat_contents['psi_hat']
+        sim.w1_hat = mat_contents['w_hat']
+        sim.convec0_hat = mat_contents['convec1_hat']
+        numbers_in_file = re.findall(r'\d+', latest_file)
+        print(numbers_in_file)
+        step = int(numbers_in_file[-1])
+        sim.stepnum = step
+        print('Step number set to', step)
+    except:
+        print('Simulation initialized')
+
     while step < nContolledSteps:
         if step % int(5e3) == 1 :
             print('Save at time step=', step)
