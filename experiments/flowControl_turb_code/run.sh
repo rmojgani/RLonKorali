d605620d27d6e29870212679b75af8e0c72b8ba5
rmojgani <rmojgani@gmail.com>
2023-05-24 01:00:54 -0500

    NumRLSteps added to file/folder name

--- experiments/flowControl_turb_code/run.sh
+++ experiments/flowControl_turb_code/run.sh
@@ -9,9 +9,10 @@ nagents=4
 nconcurrent=1
 IF_REWARD_CUM=1 #{0,1}
 Tspinup=1e3
-Thorizon=1e3
+Thorizon=1e4
+NumRLSteps=1e3
 
-myoutfile=${solver}_CASE${case}_N${NLES}_R${rewardtype}_S${statetype}_A${actiontype}_nAgents${nagents}_nCCjobs${nconcurrent}_CReward${IF_REWARD_CUM}_Ts${Tspinup}_Thor${Thorizon}.out
+myoutfile=${solver}_CASE${case}_N${NLES}_R${rewardtype}_S${statetype}_A${actiontype}_nAgents${nagents}_nCCjobs${nconcurrent}_CReward${IF_REWARD_CUM}_Ts${Tspinup}_Thor${Thorizon}_NumRLSteps${NumRLSteps}.out
 
 (echo ${myoutfile})>>${myoutfile}
 (ps)>>${myoutfile}
@@ -23,4 +24,4 @@ myoutfile=${solver}_CASE${case}_N${NLES}_R${rewardtype}_S${statetype}_A${actiont
 
 export OMP_NUM_THREADS=18
 
-nohup python3 -u run-vracer-turb.py --case=${case} --rewardtype=${rewardtype} --statetype=${statetype} --actiontype=${actiontype} --NLES=${NLES} --gensize=${gensize} --solver=${solver} --nagents=${nagents} --nconcurrent=${nconcurrent} --IF_REWARD_CUM=${IF_REWARD_CUM} --Tspinup=${Tspinup} --Thorizon=${Thorizon}>>${myoutfile}&
+nohup python3 -u run-vracer-turb.py --case=${case} --rewardtype=${rewardtype} --statetype=${statetype} --actiontype=${actiontype} --NLES=${NLES} --gensize=${gensize} --solver=${solver} --nagents=${nagents} --nconcurrent=${nconcurrent} --IF_REWARD_CUM=${IF_REWARD_CUM} --Tspinup=${Tspinup} --Thorizon=${Thorizon} --NumRLSteps=${NumRLSteps}>>${myoutfile}&
