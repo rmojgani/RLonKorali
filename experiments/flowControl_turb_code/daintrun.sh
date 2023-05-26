5132c5cf8b9056c651bbe17d431d7c5f031d4089
Daniel Waelchli <wadaniel@ethz.ch>
2023-05-26 13:26:14 +0200

    daint launch script

--- experiments/flowControl_turb_code/daintrun.sh
+++ experiments/flowControl_turb_code/daintrun.sh
@@ -2,11 +2,14 @@ NLES=32
 statetype=psiomega #psiomega # [enstrophy,energy,psidiag,psiomegadiag,psiomegalocal] 
 gensize=10
 nagents=4
-nconcurrent=2
+nconcurrent=$((${SLURM_NNODES}-1))
 Tspinup=1e3
 Thorizon=1e4
 NumRLSteps=1e3
 EPERU=1
 
+echo "Number of nodes allocated:    ${SLURM_NNODES}"
+echo "Number of concurrent workers: ${nconcurrent}"
+
 export OMP_NUM_THREADS=12
-srun --ntasks 3 python3 run-vracer-turb-mpi.py --statetype=${statetype} --NLES=${NLES} --gensize=${gensize} --nagents=${nagents} --nconcurrent=${nconcurrent} --Tspinup=${Tspinup} --Thorizon=${Thorizon} --NumRLSteps=${NumRLSteps} --EPERU ${EPERU}
+srun --nodes ${SLURM_NNODES} --ntasks-per-node 1 --threads-per-core 12 python3 run-vracer-turb-mpi.py --statetype=${statetype} --NLES=${NLES} --gensize=${gensize} --nagents=${nagents} --nconcurrent=${nconcurrent} --Tspinup=${Tspinup} --Thorizon=${Thorizon} --NumRLSteps=${NumRLSteps} --EPERU ${EPERU}
