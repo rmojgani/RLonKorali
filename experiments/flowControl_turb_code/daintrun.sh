d54c00d97bcf03c9b358fe0c1dd98efdb399be10
rmojgani <rmojgani@gmail.com>
2023-06-01 16:31:54 -0500

    output file for job submission

--- experiments/flowControl_turb_code/daintrun.sh
+++ experiments/flowControl_turb_code/daintrun.sh
@@ -8,8 +8,10 @@ Thorizon=1e4
 NumRLSteps=1e3
 EPERU=1
 
+myoutfile=MPI_${solver}_CASE${case}_N${NLES}_R${rewardtype}_S${statetype}_A${actiontype}_nAgents${nagents}_nCCjobs${nconcurrent}_CReward${IF_REWARD_CUM}_Ts${Tspinup}_Thor${Thorizon}_NumRLSteps${NumRLSteps}.out
+
 echo "Number of nodes allocated:    ${SLURM_NNODES}"
 echo "Number of concurrent workers: ${nconcurrent}"
 
 export OMP_NUM_THREADS=12
-srun --nodes ${SLURM_NNODES} --ntasks-per-node 1 --threads-per-core 12 python3 run-vracer-turb-mpi.py --statetype=${statetype} --NLES=${NLES} --gensize=${gensize} --nagents=${nagents} --nconcurrent=${nconcurrent} --Tspinup=${Tspinup} --Thorizon=${Thorizon} --NumRLSteps=${NumRLSteps} --EPERU ${EPERU}
+srun --nodes ${SLURM_NNODES} --ntasks-per-node 1 --threads-per-core 12 python3 run-vracer-turb-mpi.py --statetype=${statetype} --NLES=${NLES} --gensize=${gensize} --nagents=${nagents} --nconcurrent=${nconcurrent} --Tspinup=${Tspinup} --Thorizon=${Thorizon} --NumRLSteps=${NumRLSteps} --EPERU=${EPERU}>>${myoutfile}&
